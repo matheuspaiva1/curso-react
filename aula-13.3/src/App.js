f8a27183539df289fa498411c582bb34c951f62d
matheuspaiva1 <matheuspaiva0007@gmail.com>
2021-03-18 21:42:42 -0300

    aula-13.3

---
+++ aula-13.3/src/App.js
@@ -0,0 +1,32 @@
+import React from "react";
+import Produto from "./components/Produto";
+
+const App = () => {
+  const [dados, setDados] = React.useState(null)
+  const [loading, setLoading] = React.useState(null)
+
+  async function handleClick(event) {
+    setLoading(true)
+    const response = await fetch(
+      `https://ranekapi.origamid.dev/json/api/produto/${event.target.innerText}`,
+    )
+    const json = await response.json()
+    setDados(json)
+    setLoading(false)
+  }
+  
+  return (
+    <div>
+      <button style={{margin:'5rem'}} onClick={handleClick}>Notebook</button>
+      <button style={{margin:'5rem'}} onClick={handleClick}>Smartphone</button>
+      <button style={{margin:'5rem'}} onClick={handleClick}>Tablet</button>
+      
+      {loading && <p>Carregando...</p>}
+      {!loading && dados && <Produto dados={dados}/>}
+    </div>
+
+
+  );
+};
+
+export default App;
